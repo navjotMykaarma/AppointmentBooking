eae69f1ebceb49b32da56580b0b75e39dcc8377d
navjotMykaarma <navjot@mykaarma.com>
2024-05-31 21:43:47 +0530

    CRUD Working

--- src/main/webapp/app/appointment/appointment.service.ts
+++ src/main/webapp/app/appointment/appointment.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'environments/environment';
-import { AppointmentDTO } from 'app/appointment/appointment.model';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'environments/environment';
+import {AppointmentDTO, AppointmentRequestDTO, AppointmentResponseDTO, Page} from 'app/appointment/appointment.model';
 
 
 @Injectable({
@@ -13,14 +13,14 @@ export class AppointmentService {
   resourcePath = environment.apiPath + '/api/appointments';
 
   getAllAppointments() {
-    return this.http.get<AppointmentDTO[]>(this.resourcePath);
+    return this.http.get<Page<AppointmentResponseDTO>>(this.resourcePath);
   }
 
   getAppointment(id: number) {
     return this.http.get<AppointmentDTO>(this.resourcePath + '/' + id);
   }
 
-  createAppointment(appointmentDTO: AppointmentDTO) {
+  createAppointment(appointmentDTO: AppointmentRequestDTO) {
     return this.http.post<number>(this.resourcePath, appointmentDTO);
   }
 
